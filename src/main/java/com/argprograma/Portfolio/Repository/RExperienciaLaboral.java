e0c616fd9c556b9c84f2498f8e4711ca1337bfa9
Jaavii43 <jatoro_2001@yahoo.com.ar>
2023-01-12 16:46:33 -0300

    ddd

--- src/main/java/com/argprograma/Portfolio/Repository/RExperienciaLaboral.java
+++ src/main/java/com/argprograma/Portfolio/Repository/RExperienciaLaboral.java
@@ -2,19 +2,11 @@
 package com.argprograma.Portfolio.Repository;
 
 import com.argprograma.Portfolio.entity.ExperienciaLaboral;
-import jakarta.persistence.metamodel.SingularAttribute;
-import java.io.Serializable;
-import java.util.Optional;
-import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface RExperienciaLaboral extends JpaRepository<ExperienciaLaboral,Long>  {
-
-//    public Optional<ExperienciaLaboral> findByEmpresa(String empresa);
-
-//    public void deleteById(SingularAttribute<AbstractPersistable, Serializable> id);
-    
+ 
 }
